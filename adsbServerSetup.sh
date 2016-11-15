7483d0a85f2d6e70ccaca1c43b35a0ce8b60e30d
Mark Busby <mark@busbycreations.com>
2016-11-14 19:33:56 -0700

    Still tweaking until it works out of the box

--- adsbServerSetup.sh
+++ adsbServerSetup.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 
-pblog='/home/pi/piBakeryLog.txt'
-date &> $pblog
-echo &>> $pblog
-
 mkdir -p /home/pi/projects
 
-echo "********** dump1090 download and build" >> $pblog
-echo "git clone https://github.com/MalcolmRobb/dump1090.git /home/pi/projects/dump1090" >> $pblog 2>&1
-git clone https://github.com/MalcolmRobb/dump1090.git /home/pi/projects/dump1090 >> $pblog 2>&1
+echo "********** dump1090 download and build"
+git clone https://github.com/MalcolmRobb/dump1090.git /home/pi/projects/dump1090
 cd /home/pi/projects/dump1090/
-make >> $pblog 2>&1
+make
 
-echo "********** VirtualRadar download and start" >> $pblog
+echo "********** VirtualRadar download, extract, and start"
 mkdir -p /home/pi/projects/VirtualRadar/preview
 cd /home/pi/projects/VirtualRadar/preview
-wget http://www.virtualradarserver.co.uk/Files/Preview/VirtualRadar.tar.gz  >> $pblog 2>&1
-tar -xzf VirtualRadar.tar.gz  >> $pblog 2>&1
-wget http://www.virtualradarserver.co.uk/Files/Preview/VirtualRadar.WebAdminPlugin.tar.gz  >> $pblog 2>&1
-tar -xzf VirtualRadar.WebAdminPlugin.tar.gz  >> $pblog 2>&1
-mono /home/pi/projects/VirtualRadar/preview/VirtualRadar.exe -no-gui -createAdmin:admin -password:istrator >> $pblog 2>&1
+wget http://www.virtualradarserver.co.uk/Files/Preview/VirtualRadar.tar.gz
+tar -xzf VirtualRadar.tar.gz
+wget http://www.virtualradarserver.co.uk/Files/Preview/VirtualRadar.WebAdminPlugin.tar.gz
+tar -xzf VirtualRadar.WebAdminPlugin.tar.gz
+mono /home/pi/projects/VirtualRadar/preview/VirtualRadar.exe -no-gui -createAdmin:admin -password:istrator
